49c87bdb96928b87a3bbda89570bc6c605dd0600
Yumin <sym2596@naver.com>
2025-06-29 20:12:24 +0900

    Fix/executor (#19)
    * fix: remove debugging message
    
    * refactor: 불필요한 공백 제거

--- src/executor.c
+++ src/executor.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
-#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #include "executor.h"
 #include "parser.h"
@@ -12,14 +10,18 @@
 
 // 내부 명령어 처리
 int handle_internal_command(struct command cmd) {
+    // cd
     if (strcmp(cmd.name, "cd") == 0) {
         if (cmd.args[1] == NULL) {
             fprintf(stderr, "cd: 경로를 입력하세요.\n");
-        } else if (chdir(cmd.args[1]) != 0) {
+        } 
+        else if (chdir(cmd.args[1]) != 0) {
             perror("cd 실패");
         }
         return 1; // 내부 명령 처리 완료
     }
+
+    // pwd
     if (strcmp(cmd.name, "pwd") == 0) {
         char cwd[1024];
         if (getcwd(cwd, sizeof(cwd)) != NULL) {
@@ -29,14 +31,18 @@ int handle_internal_command(struct command cmd) {
         }
         return 1; // 내부 명령 처리 완료
     }
+
+    // export
     if (strcmp(cmd.name, "export") == 0) {
         if (cmd.args[1] == NULL) {
             fprintf(stderr, "export: 변수=값 형식으로 입력하세요.\n");
-        } else {
+        } 
+        else {
             char* equal_sign = strchr(cmd.args[1], '=');
             if (!equal_sign) {
                 fprintf(stderr, "export: 변수=값 형식이 아닙니다.\n");
-            } else {
+            } 
+            else {
                 *equal_sign = '\0';
                 char* key = cmd.args[1];
                 char* value = equal_sign + 1;
@@ -47,18 +53,22 @@ int handle_internal_command(struct command cmd) {
         }
         return 1; // 내부 명령 처리 완료
     }
+
+    // unset
     if (strcmp(cmd.name, "unset") == 0) {
         if (cmd.args[1] == NULL) {
-            fprintf(stderr, "unset : 삭제할 환경 변수를 입력하세요.\n");
-        } else{        
-        char* key = cmd.args[1];
+            fprintf(stderr, "unset: 삭제할 환경 변수를 입력하세요.\n");
+        } 
+        else {        
+            char* key = cmd.args[1];
 
-        if (unsetenv(key) != 0) {
-            perror("환경 변수 제거 실패");
-        }
-        return 1;
+            if (unsetenv(key) != 0) {
+                perror("환경 변수 제거 실패");
+            }
+            return 1;
         }
     }
+
     return 0; // 외부 명령 실행 필요
 }
 
@@ -70,7 +80,7 @@ int execute_commands(struct command* cmds, int num_cmds) {
 
     for(int i = 0; i < num_cmds; i++){
         // 마지막 명령이 아닐 때만 pipe 생성
-        if (i < num_cmds - 1){
+        if (i < num_cmds - 1) {
             if(pipe(pipe_fd) == -1){
                 perror("pipe()");
                 return -1;
@@ -78,7 +88,7 @@ int execute_commands(struct command* cmds, int num_cmds) {
         }
 
         // 내부 명령어 처리
-        if (handle_internal_command(cmds[i])){
+        if (handle_internal_command(cmds[i])) {
             if (prev_read_fd != -1) close(prev_read_fd);
             if (pipe_fd[0] != -1) close(pipe_fd[0]);
             if (pipe_fd[1] != -1) close(pipe_fd[1]);
@@ -87,7 +97,7 @@ int execute_commands(struct command* cmds, int num_cmds) {
 
         pid_t pid = fork();
 
-        if (pid < 0){
+        if (pid < 0) {
             perror("fork");
             return -1;
         }
@@ -99,12 +109,14 @@ int execute_commands(struct command* cmds, int num_cmds) {
                     perror("<");
                     exit(EXIT_FAILURE);
                 }
-            } else if (cmds[i].redirect_type == REDIRECT_OUTPUT && cmds[i].output_file) {
+            } 
+            else if (cmds[i].redirect_type == REDIRECT_OUTPUT && cmds[i].output_file) {
                 if (handle_output_redirect(cmds[i].output_file) != 0){
                     perror(">");
                     exit(EXIT_FAILURE);
                 }
-            } else if (cmds[i].redirect_type == REDIRECT_APPEND && cmds[i].output_file) {
+            } 
+            else if (cmds[i].redirect_type == REDIRECT_APPEND && cmds[i].output_file) {
                 if (handle_append_redirect(cmds[i].output_file) != 0){
                     perror(">>");
                     exit(EXIT_FAILURE);
@@ -129,44 +141,27 @@ int execute_commands(struct command* cmds, int num_cmds) {
                 exit(EXIT_FAILURE);
             }
         }
-        else{
+        else {
             if (cmds[i].is_background == 0) {
                 int status;
                 if (waitpid(pid, &status, 0) == -1) {
                     perror("waitpid failed");
                     return -1;
                 }
-
-                if (WIFEXITED(status)) {
-                    int exit_status = WEXITSTATUS(status);
-                    if (exit_status != 0) {
-                        fprintf(stderr, "'%s'를 코드 %d으로 종료되었습니다.\n", cmds[i].name, exit_status);
-                        return -1;
-                    }
-                } else if (WIFSIGNALED(status)) {
-                    int term_sig = WTERMSIG(status);
-                    fprintf(stderr, "'%s'는 신호 %d에 의해 종료되었습니다.\n", cmds[i].name, term_sig);
-                    return -1;
-                }
-            } else {
+            } 
+            else {
                 printf("[background] %d\n", pid);
-
-                // 백그라운드 자식이 좀비가 되지 않도록 SIGCHLD 무시
-                signal(SIGCHLD, SIG_IGN);
             }
 
-            if (prev_read_fd != -1){
+            if (prev_read_fd != -1) {
                 close(prev_read_fd);
             }
 
-            if (i < num_cmds - 1){
+            if (i < num_cmds - 1) {
                 close(pipe_fd[1]);
                 prev_read_fd = pipe_fd[0];
             }
-
-
         }
-
     }
 
     if (prev_read_fd != -1) {
